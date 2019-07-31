da01ec7badd7fdc5c2c043eb0f520139d951e97e
Ari Anastassiou <aristaki@me.com>
2019-07-31 22:25:38 +0100

    Small bug...

--- BERT/bert_predict_og.py
+++ BERT/bert_predict_og.py
@@ -29,8 +29,14 @@ def bert_prediction(context, response, model, tokenizer):
 @click.command()
 @click.option('--bert_model', default='bert-base-uncased', help='Batch prediction size.')
 @click.option('--batch_count', default=-1, type=int, help='Which batch to process for hacky "multiprocessing".')
-def start_inference(batch_count, bert_model):
+@click.option('--data_type', default='validation', help='Which data to use for inference.')
+def start_inference(batch_count, bert_model, data_type):
     torch.manual_seed(10)
+
+    if data_type=='testing':
+        data_location = '..data/testing_db.csv'
+    else:
+        data_location = '..data/validation_db.csv'
 
     tokenizer = BertTokenizer.from_pretrained(bert_model)
     model = BertForNextSentencePrediction.from_pretrained(bert_model)
@@ -39,21 +45,21 @@ def start_inference(batch_count, bert_model):
 
     if batch_count != -1:
         batch_size = 125000
-        df = pd.read_csv('validation_db.csv', skiprows=range(1, batch_count * batch_size + 1), nrows=batch_size)
+        df = pd.read_csv(data_location, skiprows=range(1, batch_count * batch_size + 1), nrows=batch_size)
         print(f'About to process batch number {batch_count}, which contains {df.shape[0]} samples.')
     else:
-        df = pd.read_csv('validation_db.csv')
+        df = pd.read_csv(data_location)
 
     normal_probs_single, db_probs_single = [], []
     for _, row in tqdm(df.iterrows(), total=df.shape[0]):
-        context = row.context.split('#_new_utterance_#')[-1]
+        context = row.context#.split('#_new_utterance_#')[-1]
         normal_probs_single.append(bert_prediction(context, row.response, model, tokenizer))
         db_probs_single.append(bert_prediction(context, row.db_response_new, model, tokenizer))
 
-    with open(f'part_{batch_count:02}_normal.pkl', 'wb') as f:
+    with open(f'{data_type}_full_normal.pkl', 'wb') as f:
         pickle.dump(normal_probs_single, f)
 
-    with open(f'part_{batch_count:02}_db.pkl', 'wb') as f:
+    with open(f'{data_type}_full_db.pkl', 'wb') as f:
         pickle.dump(db_probs_single, f)
 
 
