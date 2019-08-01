34ebd203f940036d4583bc893b713804c470a8cb
Ari Anastassiou <aristaki@me.com>
2019-08-01 13:25:14 +0100

    Small bug...

--- BERT/bert_predict_og.py
+++ BERT/bert_predict_og.py
@@ -14,7 +14,7 @@ def bert_prediction(context, response, model, tokenizer):
 
     # if too long, drop leading tokens
     if len(tokenized_text) > 512:
-        tokenized_text = tokenized_text[0] + tokenized_text[-511:]
+        tokenized_text = [tokenized_text[0]] + tokenized_text[-511:]
         segments_ids = segments_ids[-512:]
 
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
